d08ff0cf10b608b3d15ad88733ac2092e345dcbf
Matthew Sisley <mjsisley@gmail.com>
2017-04-23 22:24:58 -0700

    updating

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,74 @@
-require('./base')
+require('./base');
 
-const React = require('react')
-const ReactDOM = require('react-dom')
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 class Square extends React.Component {
-  render () {
+  constructor() {
+    super();
+    this.state = {
+      value: null
+    };
+  }
+  render() {
     return (
-      <button className='square'>
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
       </button>
-    )
+    );
   }
 }
 
 class Board extends React.Component {
-  renderSquare (i) {
-    return <Square />
+  renderSquare(i) {
+    return <Square value={i} />;
   }
-  render () {
-    const status = 'Next player: X'
+  render() {
+    const status = 'Next player: X';
     return (
       <div>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
+        <div className="status">{status}</div>
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className='board-row'>
+        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className='board-row'>
+        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
       </div>
-    )
+    );
   }
 }
 
 class Game extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='game'>
-        <div className='game-board'>
+      <div className="game">
+        <div className="game-board">
           <Board />
         </div>
-        <div className='game-info'>
+        <div className="game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    )
+    );
   }
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('container')
-)
+ReactDOM.render(<Game />, document.getElementById('container'));
 
-function calculateWinner (squares) {
+function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -75,12 +78,12 @@ function calculateWinner (squares) {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-  ]
+  ];
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
+    const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return squares[a];
     }
   }
-  return null
+  return null;
 }
